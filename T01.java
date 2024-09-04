b15a0fec8d203b37016459acb44fd562f5b87a7b
Choqy Pananda Sirait <krisky325@gmail.com>
2024-09-04 21:27:24 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,17 @@
 // NIM - NAMA
 // NIM - NAMA
+// 12S24012 - Choqy Pananda Sirait
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println("Nama saya adalah " + nama);
+    }
+}
 
